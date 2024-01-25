dd0ff6ad48fe702545bebbf1b351c9b63ae3bd78
Angga Sya'ban Rahid <50766782+anggasybnrhd@users.noreply.github.com>
2024-01-25 22:13:26 +0700

    Add files via upload

---
+++ jawabanno7.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int n = 5;
+        int i, j;
+        for (i = 0; i < n; i++) {
+            for (j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (j = 0; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println(" ");
+        }
+    }
+
+}
