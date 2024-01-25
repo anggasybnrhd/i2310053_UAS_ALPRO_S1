f5b81b58103ec5cef3a6667fa5fde517938e44a1
Angga Sya'ban Rahid <50766782+anggasybnrhd@users.noreply.github.com>
2024-01-25 22:03:00 +0700

    Add files via upload

---
+++ jawabanno6.java
@@ -0,0 +1,38 @@
+
+public class Main {
+    public static void main(String[] args) {
+        // Pola 1 Right Half Pyramid
+        int bintang = 5;
+        for (int b = bintang; b > 0; b--) {
+            for (int i = bintang; i >= b; i--) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // Pola 2 Reverse Right Half Pyramid
+        bintang = 5;
+        for (int b = 1; b <= bintang; b++) {
+            for (int g = bintang; g >= b; g--) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // Pola 3 Left Half Pyramid
+        bintang = 5;
+        for (int b = bintang; b > 0; b--) {
+            for (int n = 1; n <= b; n++) {
+                System.out.print("   ");
+            }
+            for (int b1 = bintang; b1 >= b; b1--) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+}
